b7b45673a1390202d990a1ffc05676a227a020aa
TheeRexGuy <rexplays1300@gmail.com>
2024-05-05 00:46:14 +0530

    Did some things.

--- cmiyc.js
+++ cmiyc.js
@@ -1,11 +1,25 @@
 document.getElementById("bt").onclick=function(){
-    document.getElementById("wndo").style.display="none";
+    document.getElementById("bt").style.display="none";
+    document.getElementById("wndo").classList.add("out");
+    document.getElementById("wndo").addEventListener("transitionend", function(){
+        console.log('Transition Ended Idiot');
+        document.getElementById("wndo").remove();
+    })
+    document.getElementById("thing").style.display="block";
 }
 
-let a, b;
-document.getElementById("box").addEventListener("mouseover", function(){
+let ad=document.getElementById("gs");
+ad.volume=0.5;
+let a, b, x, y, scr=0;
+document.getElementById("box").addEventListener("click", function(){
+    document.getElementById("sc").innerHTML=(scr++)+"";
+    ad.currentTime=0;
+    ad.play();
+    x=Math.random()*30 + 10;
     a=Math.random()*80 + 10;
     b=Math.random()*80 + 10;
+    document.getElementById("box").style.height=x+ "px";
+    document.getElementById("box").style.width=x+ "px";
     document.getElementById("box").style.top=a+ "%";
     document.getElementById("box").style.left=b+ "%";
 })
